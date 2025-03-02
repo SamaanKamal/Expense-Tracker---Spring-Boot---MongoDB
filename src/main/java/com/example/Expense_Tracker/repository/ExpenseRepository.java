ad3684e85e849dcfb6dc9769c76c00fa0bb662f8
Samaan Kamal <samaankamal72@gmail.com>
2025-03-02 12:12:21 +0200

    added find by name to get expense by name

--- src/main/java/com/example/Expense_Tracker/repository/ExpenseRepository.java
+++ src/main/java/com/example/Expense_Tracker/repository/ExpenseRepository.java
@@ -2,8 +2,13 @@ package com.example.Expense_Tracker.repository;
 
 import com.example.Expense_Tracker.model.Expense;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ExpenseRepository extends MongoRepository<Expense, String> {
+    @Query("{'name' : ?0}")
+    Optional<Expense> findByName(String Name);
 }
